19c23faa5d6e7a2f2bcf71568744d1c9fa6b9db8
Nikhil Srivastava <nikhils@banquemisr.com>
2022-09-28 19:10:40 +0530

    typo error

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "dist/apps/merchant-app-shell"));
+app.use(express.static(__dirname + "/dist/apps/merchant-app-shell"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "dist/apps/merchant-app-shell/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/apps/merchant-app-shell/index.html"));
 });
 app.listen(process.env.PORT || 5000);
