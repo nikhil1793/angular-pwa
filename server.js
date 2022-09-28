165f65d62fb5bb9c89f5b201b17a9d62535e2bcb
Nikhil Srivastava <nikhils@banquemisr.com>
2022-09-28 18:44:29 +0530

    add

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "dist/apps/merchant-app-shell"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "dist/apps/merchant-app-shell"));
+  res.sendFile(path.join(__dirname + "dist/apps/merchant-app-shell/index.html"));
 });
 app.listen(process.env.PORT || 5000);
