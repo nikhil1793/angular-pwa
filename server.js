4d6129c7e62cb8cf668c852b2e43a5df23230e8f
Nikhil Srivastava <nikhils@banquemisr.com>
2022-09-29 14:31:19 +0530

    added assetlink.json

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/apps/merchant-app-shell"));
+
+app.get("/.well-known/assetlinks.json", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/apps/merchant-app-shell/assets/assetlinks.json"));
+});
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/apps/merchant-app-shell/index.html"));
 });
